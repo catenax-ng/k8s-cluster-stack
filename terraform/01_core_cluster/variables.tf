c3d541fc348175427552199870aa01b363d8a79c
Sebastian Bezold <sebastian.bezold@daimler.com>
2022-03-22 15:32:12 +0100

    Add azure provider variables for terraform

--- terraform/01_core_cluster/variables.tf
+++ terraform/01_core_cluster/variables.tf
@@ -11,3 +11,17 @@ variable "service_principal_client_id" {
 variable "service_principal_client_secret" {
   description = "USE TF_VAR_service_principal_client_secret! The secret of the service principal that will be used to create the AKS cluster."
 }
+
+# azruerm provider variables
+variable "azure_subscription_id" {
+  description = "Subscription ID. Used for the azure provider"
+}
+variable "azure_client_id" {
+  default = "Client ID of the service principal used in azure provider"
+}
+variable "azure_client_secret" {
+  description = "Client secret of the used service principal for azrue provider"
+}
+variable "azure_tenant_id" {
+  default = "Tenant ID that should be used in azure provider"
+}
