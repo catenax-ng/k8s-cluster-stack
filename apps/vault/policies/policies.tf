3b54ad2bbefb66384c61973835a88427a1e0f4d0
Csaba Fabian <csaba.fabian@t-systems.com>
2022-04-04 12:28:23 +0000

    update engines and policies

--- apps/vault/policies/policies.tf
+++ apps/vault/policies/policies.tf
@@ -2,97 +2,97 @@ resource "vault_policy" "project_bpdm" {
   name = "project_bpdm"
 
   policy = <<EOT
-path "project-bpdm" {
+path "bpdm" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_data_format_transformer" {
-  name = "project_data_format_transformer"
+  name = "data-format-transformer"
 
   policy = <<EOT
-path "project-data-format-transformer" {
+path "data-format-transformer" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_data_integrity_demonstrator" {
-  name = "project_data_integrity_demonstrator"
+  name = "data-integrity-demonstrator"
 
   policy = <<EOT
-path "project-data-integrity-demonstrator" {
+path "data-integrity-demonstrator" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_edc" {
-  name = "project_edc"
+  name = "edc"
 
   policy = <<EOT
-path "project-edc" {
+path "edc" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_essential_services" {
-  name = "project_essential_services"
+  name = "essential-services"
 
   policy = <<EOT
-path "project-essential-services" {
+path "essential-services" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_managed_identity_wallets" {
-  name = "project_managed_identity_wallets"
+  name = "managed-identity-wallets"
 
   policy = <<EOT
-path "project-managed-identity-wallets" {
+path "managed-identity-wallets" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_material_pass" {
-  name = "project_material_pass"
+  name = "material-pass"
 
   policy = <<EOT
-path "project-material-pass" {
+path "material-pass" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_portal" {
-  name = "project_portal"
+  name = "portal"
 
   policy = <<EOT
-path "project-portal" {
+path "portal" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_semantics" {
-  name = "project_semantics"
+  name = "semantics"
 
   policy = <<EOT
-path "project-semantics" {
+path "semantics" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
 }
 
 resource "vault_policy" "project_traceability_irs" {
-  name = "project_traceability_irs"
+  name = "traceability-irs"
 
   policy = <<EOT
-path "project-traceability-irs" {
+path "traceability-irs" {
   capabilities = ["create", "read", "update", "delete", "list"]
 }
 EOT
