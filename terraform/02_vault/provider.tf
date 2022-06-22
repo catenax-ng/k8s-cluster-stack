8a063b5f636883a7d6ac824f2c71b47529618b0f
hzierer <101179191+hzierer@users.noreply.github.com>
2022-06-22 17:21:10 +0200

    moved vault config to separate module (#120)
    Co-authored-by: Harald Zierer <harald.zierer@daimler.com>

--- terraform/02_vault/provider.tf
+++ terraform/02_vault/provider.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "3.5.0"
-    }
+   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.99.0"
-    }
+    } 
   }
 
   backend "azurerm" {
