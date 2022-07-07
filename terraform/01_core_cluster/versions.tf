182331a8b931711ca6c9e462a1c4c579f098aa9a
Carsten Lenz <carslen@users.noreply.github.com>
2022-07-07 10:05:12 +0200

    add azure tenant and subscription id to provider

--- terraform/01_core_cluster/versions.tf
+++ terraform/01_core_cluster/versions.tf
@@ -8,5 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
+  client_id       = var.service_principal_client_id
+  client_secret   = var.service_principal_client_secret
+  tenant_id       = ""
+  subscription_id = ""
   features {}
 }
