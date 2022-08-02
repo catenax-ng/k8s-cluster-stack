2ebcd9f8cbbb62f409c71ea11fcb7d5d0602d7b8
Csaba Fabian <csaba.fabian@t-systems.com>
2022-08-02 14:52:12 +0000

    update config and simplify script

--- maintenance/query.py
+++ maintenance/query.py
@@ -1,71 +1,27 @@
 # Import necessary modules
 
 import yaml, json, argparse, requests
-from github import Github
 
 # Arguments for ...
 
 parser = argparse.ArgumentParser()
-parser.add_argument('gh_token')         # ... GitHub personal access token
 parser.add_argument('nr_api_key')       # ... newreleases.io api key
 args = parser.parse_args()
 
 # Variables for ...
 
-gh_token = args.gh_token                # ... GitHub personal access token
 nr_api_key = args.nr_api_key            # ... newreleases.io api key
 results = []
 
 # Define functions
 
-def query_apis(path, dep, proj, pref):
-
-# Query github api
-
-  g = Github(gh_token)
-  repo = g.get_repo(configjson['github_repo'])
-  if('Chart.yaml' in path):
-    print('this is a helm chart')
-    chartyaml = yaml.safe_load(repo.get_contents(path).decoded_content.decode())
-    app_name = chartyaml['name']
-    app_version = chartyaml['version']
-    try:
-      dependencies = (chartyaml['dependencies'])
-    except KeyError:
-      dependencies = 'dependencies'
-    if (dependencies != 'dependencies'):
-      app_name = dependencies[dep]['name']
-      app_version = dependencies[dep]['version']
-    #result = {'name': chart_name, 'deployed': chart_version, 'latest': ''}
-  elif('kustomization.yaml' in path):
-    print('this is a kustomization')
-    kustomization = repo.get_contents(path).decoded_content.decode()
-    kustomizationyaml = yaml.safe_load(kustomization)
-    app_name = kustomize['app']
-    app_version = kustomizationyaml['resources'][1].split('/')[5].replace('v','')
-    #result = {'name': app_name, 'deployed': app_version, 'latest': ''}
-  elif('argo-vault-plugin.yaml' in path):
-    print('this is a deployment')
-    k8s_deployment = repo.get_contents(path).decoded_content.decode()
-    k8s_deploymentyaml = yaml.safe_load(k8s_deployment)
-    app_name = deployment['app']
-    app_version = k8s_deploymentyaml['spec']['template']['spec']['initContainers'][0]['env'][0]['value']
-    #result = {'name': app_name, 'deployed': app_version, 'latest': ''}
-  #print(result)
-#  results.append(result)
+def query_versions(name, dep, proj, pref):
 
 # Query newreleases api
 
   kps_releases = []
   nr_api_url = configjson['newreleases_api_url']
   projects_url = f'{nr_api_url}/projects'
-#  response_projects = requests.get(projects_url,
-#    headers = {'Content-Type':'application/json',
-#               'X-Key':nr_api_key})
-#  response_projects.raise_for_status()
-#  jsonResponseProjects = response_projects.json()
-#  r_ps = json.dumps(jsonResponseProjects)
-#  projects = json.loads(r_ps)
   # search for project
   search_url = f'{projects_url}/search?q={proj}'
   response_search = requests.get(search_url,
@@ -73,7 +29,6 @@ def query_apis(path, dep, proj, pref):
                'X-Key':nr_api_key})
   response_search.raise_for_status()
   json_response_search = response_search.json()
-  #print(json_response_search)
   # get id of project
   projectid = json_response_search['projects'][0]['id']
   # query project releases
@@ -89,33 +44,24 @@ def query_apis(path, dep, proj, pref):
       if 'kube-prometheus-stack' in rel['version']:
         kps_releases.append(rel)
     latest = kps_releases[0]
-  latestversion = latest.get('version').rpartition('-')[-1] or latest.get('version')
-  result = {'name': app_name, 'deployed': app_version, 'latest': latestversion}
+  latestversion = latest.get('version').replace(pref,'') or latest.get('version')
+  result = {'name': name, 'deployed': dep, 'latest': latestversion}
   results.append(result)
 
-  # select latest release (+remove prefix?)
-
-
-# def query_newreleases(proj, pref) # ... query newreleases
-
-# ... query newreleases api
+# Open the config
 
 config = open('config.json')
 configjson = json.load(config)
 
-charts = configjson['charts']
-for chart in charts:
-  print(chart['app'])
-  query_apis(chart['path'], chart['dependency'], chart['project'], chart['prefix'])
+# Get the apps list
 
-kustomizes = configjson['kustomizes']
-for kustomize in kustomizes:
-  print(kustomize['app'])
-  query_apis(kustomize['path'], kustomize['dependency'], kustomize['project'], kustomize['prefix'])
+apps = configjson['apps']
 
-deployments = configjson['deployments']
-for deployment in deployments:
-  print(deployment['app'])
-  query_apis(deployment['path'], deployment['dependency'], deployment['project'], deployment['prefix'])
+# Make query for each app
+
+for app in apps:
+  query_versions(app['name'], app['deployed'], app['project'], app['prefix'])
+
+#Print the results (change this to prometheus exporter)
 
 print(results)
