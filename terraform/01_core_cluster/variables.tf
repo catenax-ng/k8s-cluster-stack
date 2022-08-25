df21ca936ba0e500badc044722bf3bb78f24274f
GRIGARS <steffen.grigar@daimler.com>
2022-08-25 14:01:01 +0200

    update argocd cluster

--- terraform/01_core_cluster/variables.tf
+++ terraform/01_core_cluster/variables.tf
@@ -1,3 +1,33 @@
+# Adjust variables according to cluster specifics
+#
+variable "environment_name" {
+  description = "Name of the environment to create, i.e. 'core'. Will be used in several resource names"
+  type        = string
+  default     = "core"
+}
+
+variable "k8s_vm_size" {
+  description = "The Azure VM Size string i.e. Standard_D2_v2 or Standard_D8s_v3"
+  type        = string
+  default     = "Standard_D8s_v4"
+}
+
+variable "k8s_cluster_node_count" {
+  description = "The number of kubernetes nodes to create for the k8s cluster"
+  type        = number
+  default     = 3
+}
+
+variable "k8s_version" {
+  description = "AKS k8s Version to deploy"
+  type        = string
+  default     = "1.22"
+}
+
+##################################################################
+# DO NOT CHANGE VARIABLES BELOW THIS LINE!
+#
+
 variable "azure_tenant_id" {
   description = "Azure TenantID to use"
   type        = string
@@ -13,11 +43,6 @@ variable "azure_dns_subscription_id" {
   type        = string
 }
 
-variable "environment_name" {
-  description = "Name of the environment to create, i.e. 'core'. Will be used in several resource names"
-  type        = string
-}
-
 variable "service_principal_client_id" {
   description = "USE TF_VAR_service_principal_client_id! The client ID of the service principal that will be used to create the AKS cluster."
   type        = string
@@ -25,20 +50,4 @@ variable "service_principal_client_id" {
 
 variable "service_principal_client_secret" {
   description = "USE TF_VAR_service_principal_client_secret! The secret of the service principal that will be used to create the AKS cluster."
-}
-variable "k8s_version" {
-  description = "AKS k8s Version to deploy"
-  type        = string
-}
-
-variable "k8s_vm_size" {
-  description = "The Azure VM Size string i.e. Standard_D2_v2 or Standard_D8s_v3"
-  type        = string
-  default     = "Standard_D8s_v3"
-}
-
-variable "k8s_cluster_node_count" {
-  description = "The number of kubernetes nodes to create for the k8s cluster"
-  type        = number
-  default     = 3
 }
